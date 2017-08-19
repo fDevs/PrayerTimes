987e5d5e14c98b89950e4c855a396680d968e4d2
Imran Khan <imranak123@gmail.com>
2017-08-18 20:36:43 -0600

    intial project PrayerTimes

---
+++ PrayerTimes/Models/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrayerTimes.Models
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        
+    }
+}
---
+++ PrayerTimes/Models/MasjidUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrayerTimes.Models
+{
+    public class MasjidUser
+    {
+        public int MasjidUserId { get; set; }
+    }
+}
