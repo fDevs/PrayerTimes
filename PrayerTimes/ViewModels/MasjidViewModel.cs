5d1532a9c1458a76ddf0d8025e404c17d91a204d
Taiyyab <hafiztaiyyab11@gmail.com>
2017-09-09 10:32:27 -0500

    Added: Date time objects for namaz times Masjid View Model rendered example content on a view updated home controller to return the view model. ISSUE 1 COMPLETED BOYZ

---
+++ PrayerTimes/ViewModels/MasjidViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PrayerTimes.Models;
+
+namespace PrayerTimes.ViewModels
+{
+    public class MasjidViewModel
+    {
+        public List<Masjid> Masjids { get; set; }
+
+    }
+}
